cb320e7aa0c6991eebf5e8addf1033af031a5679
Anish jha <122715402+anish-stack@users.noreply.github.com>
2024-03-29 12:10:11 +0530

    Update route.js

--- Backend/routers/route.js
+++ Backend/routers/route.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const { createProduct,getallproduct,deleteProduct,singleProduct } = require('../controllers/productController');
+const { getContacts, createContact, deleteMail } = require('../controllers/ContactController');
 const router = express.Router();
 // productController.createProduct
 router.post('/createProducts',createProduct )
 router.get('/allProduct', getallproduct)
 router.delete( '/deleteProduct/:id', deleteProduct);
 router.get( '/singleProduct/:id', singleProduct);
+router.get( '/getcontact', getContacts);
+router.post( '/createcontact', createContact);
+router.delete('/delete-mail/:id', deleteMail)
 
 
 module.exports = router;
